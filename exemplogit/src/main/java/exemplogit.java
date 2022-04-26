38bd5d6234688683c63ecf3593decd82048370a2
guuhmendes <gustavomendes2402942@hotmail.com>
2022-04-25 22:04:42 -0300

    criação de novo projeto java

---
+++ exemplogit/src/main/java/exemplogit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author gustavo.mdelmondes
+ */
+public class exemplogit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        int soma = 1 +1;
+        System.out.println("Resultado soma; " + soma);
+    }
+    
+}
